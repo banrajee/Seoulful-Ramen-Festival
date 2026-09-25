1bdbdd20d0ab73505a9177f2a97ced10b283b945
banrajee <aibanrajee01@gmail.com>
2026-09-25 14:56:20 +0530

    Add dedicated festival admin route

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { FestivalAdmin } from "@/components/festival-admin";
+
+export const metadata = { title: "Festival Admin | Seoulful Ramen" };
+
+export default function AdminPage() {
+  return <FestivalAdmin />;
+}
+
